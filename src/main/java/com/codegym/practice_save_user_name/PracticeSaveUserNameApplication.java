7b8a83fff9a786336b499cf1904f2ed7a74c2ae5
GohanVu <hoangvhdata@gmail.com>
2024-07-27 09:03:52 +0700

    update

---
+++ src/main/java/com/codegym/practice_save_user_name/PracticeSaveUserNameApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.practice_save_user_name;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSaveUserNameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeSaveUserNameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/practice_save_user_name/PracticeSaveUserNameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.practice_save_user_name;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSaveUserNameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
